1f38e1bf84154c2845d7c12f84a3d185b24913c6
Jordan Paris <hamologist@gmail.com>
2021-02-13 23:45:44 -0500

    Set json input and output to lowercase

--- pkg/model/emojify_payload.go
+++ pkg/model/emojify_payload.go
@@ -1,5 +1,5 @@
 package model
 
 type EmojifyPayload struct {
-	Message string
+	Message string `json:"message"`
 }
--- pkg/model/emojify_response.go
+++ pkg/model/emojify_response.go
@@ -1,5 +1,5 @@
 package model
 
 type EmojifyResponse struct {
-	Message string
+	Message string `json:"message"`
 }
